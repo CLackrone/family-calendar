b3391552fb3a228b9356b71cffe7af805ee18c5a
CLackrone <charisaanne@gmail.com>
2017-09-18 09:22:39 -0400

    category show page, link from index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,18 @@
 class CategoriesController < ApplicationController
 
+  before_action :set_category, only: [:show]
+
   def index
     @categories = Category.all
   end
 
-  
+  def show
+  end
+
+  private
+
+  def set_category
+    @category = Category.find_by(id: params[:id])
+  end
 
 end
