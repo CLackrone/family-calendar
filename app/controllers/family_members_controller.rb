bdf7c401516fc7a525501dcd24c5260b401ea98a
CLackrone <charisaanne@gmail.com>
2017-09-18 09:52:41 -0400

    family member index and show views and controller

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -1,6 +1,22 @@
 class FamilyMembersController < ApplicationController
 
+  before_action :set_member, only: [:show]
+
+  def index
+    @members = FamilyMember.all 
+  end
+
   def new
+  end
+
+  def show
+  end
+
+  private
+
+  def set_member
+    @member = FamilyMember.find_by(id: params[:id])
   end
   
 end
+
