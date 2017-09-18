c797f361132e2e28e33f2a1ed8cebde32e0d7c32
CLackrone <charisaanne@gmail.com>
2017-09-18 14:42:30 -0400

    family member create controller action

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -7,10 +7,13 @@ class FamilyMembersController < ApplicationController
   end
 
   def new
-    @member = FamilyMember.new
+    @member = FamilyMember.new(user_id: current_user.id)
   end
 
   def create
+    @member = current_user.family_members.new(member_params)
+    @member.save
+    redirect_to family_member_path(@member)
   end
 
   def show
@@ -21,6 +24,9 @@ class FamilyMembersController < ApplicationController
   def set_member
     @member = FamilyMember.find_by(id: params[:id])
   end
+
+  def member_params
+    params.require(:family_member).permit(:name, :age, :user_id)
+  end
   
 end
-
