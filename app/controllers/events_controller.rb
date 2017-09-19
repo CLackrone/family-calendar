6b29fb469770d59b27bcb21bef080e05470cc681
CLackrone <charisaanne@gmail.com>
2017-09-19 14:37:53 -0400

    events new form and controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,12 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      redirect_to new_event_path
+    end
   end
 
   def index
@@ -17,6 +23,10 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def event_params
+    params.require(:event).permit(:name, :time, :location, :category_id)
+  end
 
   def set_event
     @event = Event.find_by(id: params[:id])
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :category
   has_many :family_member_events
   has_many :family_members, through: :family_member_events
+
+  validates :name, :date, :location, presence: true
 end
