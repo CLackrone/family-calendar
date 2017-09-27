6eeb52fb36e74250127904e8689f1ce09a9769f7
CLackrone <charisaanne@gmail.com>
2017-09-27 11:38:24 -0400

    added omniauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :family_members
   resources :events
   devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
