1e45a19e9f5104bf3a2b919e9b2ab0602569a73a
CLackrone <charisaanne@gmail.com>
2018-01-10 08:52:13 -0500

    Added events.js, setting up files to add jquery frontend

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,12 @@
+//$(document).ready(function() {
+//  $('.testerbutton').on('click', function() {
+//    $.ajax({
+//      url: '/events.json',
+//      method: 'GET'
+//    })
+//  })
+//})
+
+$(document).ready(function() {
+  $('#eventsInfo').append("This is displaying in the right place. We are going to try again.")
+})
