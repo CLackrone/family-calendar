14e678e48da882fc1965f595935b40aebe8a2cdb
CLackrone <charisaanne@gmail.com>
2018-01-11 14:03:45 -0500

    $.each working for eventsArray

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,18 +1,28 @@
 $(function() {
   var eventsArray = [];
-  // var id = parseInt($(".js-next").attr("data-id"));
 
   if ($('#eventInfo').length) {
     loadEvents();
   }
 
+  // For events#index
+
   function loadEvents() {
+    //ajax call 
     $.ajax({
       url: '/events.json',
       method: 'GET'
     })
     .then(function(data) {
       console.log(data)
+      eventsArray = data;
+
+      //jQuery.each( array, callback )
+      $.each(
+        eventsArray, function(index, eachEvent) {
+          console.log(eachEvent)
+        }
+        )
     })
   }
 
