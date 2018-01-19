6183121a331bba0aee884a3047e78eb7252826d3
CLackrone <charisaanne@gmail.com>
2018-01-19 13:05:32 -0500

    migration for Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011200454) do
+ActiveRecord::Schema.define(version: 20180119180323) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
