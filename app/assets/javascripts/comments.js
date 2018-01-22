b4b5a9cbf9e5ea4ccf52acbb754ebdcc232a15fa
CLackrone <charisaanne@gmail.com>
2018-01-22 13:39:56 -0500

    render comments#index without layout

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,6 +2,11 @@ $(function() {
   //event handler for comments#index on event#show
   $('a.load_comments').on('click', function(e) {
     e.preventDefault();
-    console.log("You clicked this link!!!!!!!!!");
+    $.ajax({
+      method: 'GET',
+      url: this.href
+    }).done(function(data) {
+      console.log(data)
+    })
   })
 })
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,6 @@ class CommentsController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
     @comments = @event.comments
+    render 'comments/index', :layout => false
   end
 end
